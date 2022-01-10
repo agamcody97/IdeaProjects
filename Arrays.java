94c474f19268332c289f9d1a381b3b91279e90b8
agamcody97 <agamsharma2897@gmail.com>
2022-01-09 20:51:26 -0500

    program done

--- Arrays.java
+++ Arrays.java
@@ -1,7 +1,19 @@
 package com.agam_programs;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Arrays {
     public static void main(String[] args) {
-
+        int n;
+        ArrayList<Integer> list=new ArrayList<Integer>(10);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of elements you want to store in the array list");
+        n=sc.nextInt();
+        System.out.println("Enter "+n+" elements");
+        for (int i = 1; i <= n; i++) {
+            list.add(sc.nextInt());
+        }
+        System.out.println(list);
     }
 }
--- SumOfNaturalNo.java
+++ SumOfNaturalNo.java
@@ -1,9 +1,9 @@
-/*
+package com.agam_programs;/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication3;
+
 
 import java.util.Scanner;
 
