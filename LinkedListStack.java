f54beba1da00231f365d947f945c899916ee1861
agamcody97 <agamsharma2897@gmail.com>
2022-01-15 17:13:06 -0500

    completed

--- LinkedListStack.java
+++ LinkedListStack.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package linkedliststack;
+package com.agam_programs;
 
 import java.util.LinkedList;
 import java.util.Scanner;
